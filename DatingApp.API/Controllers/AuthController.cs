8a9f8fe2ff2bf6ed651e2f02a93c51f215eb0230
panxman <nentzos.pan@gmail.com>
2020-08-16 18:28:07 +1000

    User Auth on the client side

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -49,6 +49,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            userForLoginDto.Username = userForLoginDto.Username.ToLower();
+            
             var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
 
             if (userFromRepo == null)
